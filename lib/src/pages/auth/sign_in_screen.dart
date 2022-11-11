14e6e802adf38b13d6ab83ed80a59b00b693a69d
Joao Felipe <joaofelipedev01@gmail.com>
2022-11-11 15:53:12 -0300

    first commit

---
+++ lib/src/pages/auth/sign_in_screen.dart
@@ -0,0 +1,179 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:quitanda/src/pages/auth/sign_up_screen.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
+
+import '../base/base_screen.dart';
+import 'components/custom_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //Nome do App
+                    Text.rich(TextSpan(
+                        style: const TextStyle(
+                          fontSize: 40,
+                        ),
+                        children: [
+                          const TextSpan(
+                              text: 'Green',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          TextSpan(
+                              text: 'grocer',
+                              style: TextStyle(
+                                  color: CustomColors.customContrastColor))
+                        ])),
+
+                    // Categorias
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 25),
+                        child: AnimatedTextKit(
+                          pause: Duration.zero,
+                          repeatForever: true,
+                          animatedTexts: [
+                            FadeAnimatedText('Frutas'),
+                            FadeAnimatedText('Verduras'),
+                            FadeAnimatedText('Legumes'),
+                            FadeAnimatedText('Carnes'),
+                            FadeAnimatedText('Cereais'),
+                            FadeAnimatedText('Laticineos'),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // Formulario
+
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(45))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
+                    CustomTextField(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isSecret: true,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        )),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacement(MaterialPageRoute(builder: (c) {
+                            return BaseScreen();
+                          }));
+                        },
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text(
+                              'Ou',
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (c) {
+                            return SignUpScreen();
+                          }));
+                        },
+                        // ignore: prefer_const_constructors
+                        // ignore: sort_child_properties_last
+                        child: Text(
+                          'Criar Conta',
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.green,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/home/home_tab.dart
@@ -0,0 +1,155 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
+import 'package:quitanda/src/config/app_data.dart' as appData;
+import 'components/category_tile.dart';
+import 'components/item_tile.dart';
+
+class HomeTab extends StatefulWidget {
+  const HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  String selectedCategory = 'Frutas';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // App Bar
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+            children: [
+              TextSpan(
+                text: 'Green',
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: 'grocer',
+                style: TextStyle(
+                  color: CustomColors.customContrastColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+
+      // Campo de pesquisa
+
+      body: Column(
+        children: [
+          // Campo de pesquisa
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Categorias
+          Container(
+            padding: const EdgeInsets.only(left: 15),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  category: appData.categories[index],
+                  isSelected: appData.categories[index] == selectedCategory,
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = appData.categories[index];
+                    });
+                  },
+                );
+              },
+              separatorBuilder: (_, index) {
+                return const SizedBox(
+                  width: 10,
+                );
+              },
+              itemCount: appData.categories.length,
+            ),
+          ),
+          // Grid
+
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 9 / 11.5),
+              itemCount: appData.items.length,
+              itemBuilder: (_, index) {
+                return ItemTile(
+                  item: appData.items[index],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
